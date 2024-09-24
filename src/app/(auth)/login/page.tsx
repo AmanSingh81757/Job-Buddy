abef5eddf7100a6583f31de141e7fc12ff4da0a7
Aman Singh <amansingh81757@gmail.com>
2024-09-24 18:38:49 +0530

    feat: add login and signup pages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LogInFormCard from "@/components/auth/login-form";
+
+export default function LogInPage() {
+    return (
+        <section className="w-full h-screen flex items-center justify-center bg-gradient-to-tl from-purple-100 to-blue-100 p-5">
+            <LogInFormCard />
+        </section>
+    )
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignUpFormCard from "@/components/auth/signup-form";
+
+export default function SignUpPage() {
+    return (
+        <section className="w-full h-screen flex items-center justify-center bg-gradient-to-tl from-purple-100 to-blue-100 p-5">
+            <SignUpFormCard />
+        </section>
+    )
+}
