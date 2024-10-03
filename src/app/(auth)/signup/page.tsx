0cfcf70fc8347b148f149cfdbd7d46551b05996f
Aman Singh <amansingh81757@gmail.com>
2024-10-03 23:02:50 +0530

    feat: add github and google auth

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,61 @@
-import SignUpFormCard from "@/components/auth/signup-form";
+"use client";
+
+import { Github } from "lucide-react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signIn } from "next-auth/react";
 
 export default function SignUpPage() {
-    return (
-        <section className="w-full h-screen flex items-center justify-center bg-gradient-to-tl from-purple-100 to-blue-100 p-5">
-            <SignUpFormCard />
-        </section>
-    )
+  return (
+    <section className="w-full h-screen flex items-center justify-center bg-gradient-to-tl from-purple-100 to-blue-100 p-5">
+      <Card className="shadow-xl">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl">Create an account</CardTitle>
+          <CardDescription>
+            Enter your email below to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <form className="grid gap-4">
+          <div className="grid gap-2">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" type="text" placeholder="Full Name" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="email@example.com" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" placeholder="password" />
+            </div>
+            <Button className="w-full bg-teal-600 text-white hover:bg-teal-700">
+              Create account
+            </Button>
+          </form>
+        </CardContent>
+        <CardFooter className="flex flex-col gap-5">
+          <p>
+            Already have an account?{" "}
+            <Link
+              href="/login"
+              className="underline font-bold text-teal-600 hover:text-teal-700"
+            >
+              Log In
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </section>
+  );
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,21 +1,26 @@
 import type { Metadata } from "next";
-import Sidebar from '@/components/dashboard/sidebar'
-import DashboardHeader from '@/components/dashboard/dashboardHeader'
+import Sidebar from "@/components/dashboard/sidebar";
+import DashboardHeader from "@/components/dashboard/dashboardHeader";
+import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   title: "Job Buddy auth",
   description: "authentication page for job buddy",
 };
 
-
-export default async function DashboardClient({ children }: { children: React.ReactNode }) {
-
+export default async function DashboardClient({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="flex h-screen overflow-hidden">
-      <aside className="hidden w-64 border-r bg-gray-100/40 dark:bg-gray-800/40 md:block">
-        <Sidebar />
-      </aside>
-      <DashboardHeader>{children}</DashboardHeader>
-    </div>
-  )
+    <SessionProvider>
+      <div className="flex h-screen overflow-hidden">
+        <aside className="hidden w-64 border-r bg-gray-100/40 dark:bg-gray-800/40 md:block">
+          <Sidebar />
+        </aside>
+        <DashboardHeader>{children}</DashboardHeader>
+      </div>
+    </SessionProvider>
+  );
 }
